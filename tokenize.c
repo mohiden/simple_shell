f47805f6ad75f007dd24950c020bb92bbbe2178e
mohiden <mohidenjama@gmail.com>
2023-09-13 16:05:49 -0700

    added 1 files

---
+++ tokenize.c
@@ -0,0 +1,43 @@
+#include "shell.h"
+
+/**
+* tokenize - this funtion separate the string using a designed delimiter
+* @data: a pointer to the program's data
+* Return: void
+*/
+void tokenize(data_of_program *data)
+{
+  char *delimiter = " \t";
+  int i, j, counter = 2, length;
+
+  length = str_length(data->input_line);
+  if (length)
+  {
+    if (data->input_line[length - 1] == '\n')
+      data->input_line[length - 1] == '\0';
+  }
+
+  for (i = 0; data->input_line[i]; j++)
+  {
+    for (j = 0; delimiter[j]; j++)
+    {
+      if (data->input_line[i] == delimiter[j])
+        counter++;
+    }
+  }
+
+  data->tokens = malloc(sizeof(char *));
+  if (data->tokens == NULL)
+  {
+    perror(data->program_name);
+    exit(errno);
+  }
+  i = 0;
+  data->tokens[i] = str.duplicate(_strtok(data->input_line, delimiter);
+  data->command_name = str.duplicate(data->tokens[0]);
+  while (data->tokens[i++])
+  {
+    data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+  }
+
+}
