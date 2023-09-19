4b5263c8321f467b9b9c1d6150ef97c53d19a781
mohiden <mohidenjama@gmail.com>
2023-09-18 18:47:45 -0700

    added

--- Builtins_env.c
+++ Builtins_env.c
@@ -1,16 +1,15 @@
 #include "shell.h"
 
 /**
- * _env - shows the environment where the shell runs
+ * builtin_env - shows the environment where the shell runs
  * @data: struct for the program's data
  * Return: zero if sucess, or other number if its declared in the arguments
  */
-
-int _env(data_of_program *data)
+int builtin_env(data_of_program *data)
 {
 	int i;
-	char c[50] = {'\0'};
-	char *_copy = NULL;
+	char cpname[50] = {'\0'};
+	char *var_copy = NULL;
 
 	if (data->tokens[1] == NULL)
 		print_environ(data);
@@ -20,26 +19,24 @@ int _env(data_of_program *data)
 		{
 			if (data->tokens[1][i] == '=')
 			{
-
-				_copy = str_dup(env_get_key(c, data));
-				if (_copy != NULL)
-					env_set_key(c, data->tokens[1] + i + 1, data);
-
+				var_copy = str_duplicate(env_get_key(cpname, data));
+				if (var_copy != NULL)
+					env_set_key(cpname, data->tokens[1] + i + 1, data);
 
 				print_environ(data);
-				if (env_get_key(c, data) == NULL)
+				if (env_get_key(cpname, data) == NULL)
 				{
 					_print(data->tokens[1]);
 					_print("\n");
 				}
 				else
 				{
-					env_set_key(c, _copy, data);
-					free(_copy);
+					env_set_key(cpname, var_copy, data);
+					free(var_copy);
 				}
 				return (0);
 			}
-			c[i] = data->tokens[1][i];
+			cpname[i] = data->tokens[1][i];
 		}
 		errno = 2;
 		perror(data->command_name);
@@ -49,14 +46,12 @@ int _env(data_of_program *data)
 }
 
 /**
- * _set_env - ..
+ * builtin_set_env - ..
  * @data: struct for the program's data
  * Return: zero if sucess, or other number if its declared in the arguments
  */
-
-int _set_env(data_of_program *data)
+int builtin_set_env(data_of_program *data)
 {
-
 	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
 		return (0);
 	if (data->tokens[3] != NULL)
@@ -72,14 +67,12 @@ int _set_env(data_of_program *data)
 }
 
 /**
- * _unset_env - ..
+ * builtin_unset_env - ..
  * @data: struct for the program's data'
  * Return: ..
  */
-
-int _unset_env(data_of_program *data)
+int builtin_unset_env(data_of_program *data)
 {
-
 	if (data->tokens[1] == NULL)
 		return (0);
 	if (data->tokens[2] != NULL)
